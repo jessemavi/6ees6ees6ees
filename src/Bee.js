fd2a753ae5314b2a1b6db69c9d95466030a05378
Jesse Mavi <jesse.mavi@gmail.com>
2016-09-19 11:27:25 -0700

    Completes Bee class

--- src/Bee.js
+++ src/Bee.js
@@ -1,7 +1,13 @@
 import Grub from './Grub'
 
-class Bee {
+class Bee extends Grub {
   // TODO..
+  constructor (age, color, food, eat, job) {
+    super();
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'Keep on growing';
+  }
 };
 
 export default Bee;
